96eebe354e0f9ce98f1e810ade7942de879f0b71
inga_demetrashvili_at_488565689350 <inga@gantabi.com>
2024-06-03 13:17:34 +0200

    Correction - catch ArrayOutOfBounds

--- src/main/java/org/example/n1exercici3/Main.java
+++ src/main/java/org/example/n1exercici3/Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-
         error();
-        } catch (RuntimeException e){
+        } catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Array out of index");
         }
 
